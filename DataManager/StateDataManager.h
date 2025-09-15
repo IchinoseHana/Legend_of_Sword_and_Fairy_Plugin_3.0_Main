514217f67cc5afd47c1525e20af656c45f40ec98
Miku <53478560+mlisgood@users.noreply.github.com>
2025-09-16 00:12:13 +0900

    [Code Refactoring] Feat: Write a part of the data structure about the state slot 1 and 2

--- DataManager/StateDataManager.h
+++ DataManager/StateDataManager.h
@@ -1,86 +1,95 @@
 #ifndef _PAL3_STATEDATAMANAGER_H_
 #define _PAL3_STATEDATAMANAGER_H_
 
+const static int stateMaximumEffectIdentifier = 10; // The maximum size of the effect identifier
+
+enum class StateCustomizedTriggerType 
+{
+    StateCustomizedTriggerTypeByRound, // Triggered: At the start of each round
+    StateCustomizedTriggerTypeAfterNormalAttack, // Triggered: After doing a normal attack
+	StateCustomizedTriggerTypeAfterSkill, // Triggered: After casting a skill
+	StateCustomizedTriggerTypeAfterMagic, // Triggered: After casting a magic
+	StateCustomizedTriggerTypeAfterItem, // Triggered: After using an item
+	StateCustomizedTriggerTypeAfterDefense, // Triggered: After defending
+	StateCustomizedTriggerTypeAfterRecievingDamage, // Triggered: After receiving damage
+};
+
 #pragma once
 
-// State Slot One
-// State -> m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonWind]
-// Duration -> m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonThunder]
-
-// Define the state slot one
-// States from enemies and items. If the target is a role of the player, it cannot be removed except waiting for elimination natually or using some special items
-// If the state is used to hurt enemies, it will not disrupt the current action of the target
-// For the marker at the end: "_D" means the state will use the second parameter defined by "m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonWater]", "_T" means the state is just used for coding, "_L[Number]" is used to prevent the duplicated symbols for states with the similar effect
-
-
-// State Slot Two
-// State -> m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonFire]
-// Duration -> m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonEarth]
-
-// Define the state slot two
-// States from players and items which can be removed or replaced easily. However, if the target is an enemy, it is impossible to remove or replace this
-// For the marker at the end: "_D" means the state will use the second parameter defined by "m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonWater]", "_T" means the state is just used for coding, "_L[Number]" is used to prevent the duplicated symbols for states with the similar effect
-// "_S" will be added when the state should be implemented by specific ways in the code
-
-
-// State Slot Three
-// State -> byAbsorbHPWormRoundExtend[nRoleIndex]
-
-// Define the state slot one
-// States just be effective once or a specific number of times
-// Almost used by players
-// For the marker at the end: "_D" means the state will use the second parameter defined by "m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonWater]", "_T" means the state is just used for coding, "_L[Number]" is used to prevent the duplicated symbols for states with the similar effect
-
-	
 class StateDataManager
 {
 public:
-
-	// Use a list of external files to manage datas.
-    // The default value is zero. Use a negative value to achieve the contrast effect.
- 
-	// MARK: State Slot One
-    // State -> m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonWind]
-    // Duration -> m_Role[nRoleIndex].snSpecStateLurk[CBRSS_PoisonThunder]
-
-    // Define the state slot one.
-    // States from enemies and items. If the target is a role of the player, it cannot be removed except waiting for elimination natually or using some special items.
-    // If the state is used to hurt enemies, it will not disrupt the current action of the target.
-	int identifier; // The ID of the state.
-	char *name; // The name of the state.
-	char *description; // The description of the state.
-	unsigned int effectIdentifier[10]; // The effect identifier when triggered
-	unsigned int layer; // The max number of layers for a specific state. A effect of a state will be multiplied by this variable. The value 0 takes the same effect as 1.
-	
-	// MARK: Detailed effects
-	// {type} -> normal attack, skill, magic, item, sword skill, all
-    // {property} -> water, fire, wind, thunder, earth
-	// {state1} -> current HP, current SP, current MP, money
-	// {state2} -> HP, SP, MP, attack, defense, speed, luck
-
-	// Triggered: At a specific time
-	int dealedDamageModificationByPercent[6]; // Dealed {type} damage + v% 
-	int dealedMagicDamageModificationByPercentWithProperty[5]; // Dealed {property} magic damage + v% 
-	int receivedDamageModificationByPercent[6]; // Recieved {type} damage + v%
-	int receivedMagicDamageModificationPercentWithProperty[5]; // Recieved {property} magic damage + v%
-	int stateModification[7]; // {state2} + v
-	int stateModificationByPercent[7]; // {state2} + v%
-	int actionTakesNoEffect[6]; // {type} based action takes no effect
-
-	// Triggered: At the start of the each round
-	int stateModificationByRoundFixed[4]; // At the start of the round: {state1} + v
-    int stateModificationByRoundFixedPercent[4]; // At the start of the round: {state1} + v%
-    int stateModificationByRoundLevelBased[3]; // At the start of the round: {state1} + v
-	
-
-	// Special effects
-
 
 	StateDataManager();
 	~StateDataManager();
 };
 
+struct StateDataInstance
+{
+public:
 
+	// Use a list of external files to manage datas.
+    // The default value is 0 or false. Use a negative value to achieve the contrast effect.
 
+    // If the state is used to hurt enemies, it will not disrupt the current action of the target.
+	int identifier; // The ID of the state.
+	char *name; // The name of the state.
+	char *description; // The description of the state.
+	unsigned int effectIdentifier[stateMaximumEffectIdentifier]; // The effect identifier when triggered.
+	unsigned int layer; // The maximum number of layers for a specific state. An effect of a state will be multiplied by the value of the current layer. The value 0 takes the same effect as 1. Slot 2: States from players and items which can be removed or replaced easily. However, if the target is an enemy, it is impossible to be removed or replaced.
+    unsigned int duration; // The duration of the state.
+	unsigned int step; // The step of the value. After being triggered, the value will be added by this value.
+	unsigned int delay; // The count before the state taking effect.
+	unsigned int slot; // The slot of the state. Slot 1: states from enemies and items. If the target is a role of the player, it cannot be removed except waiting for elimination natually or using some special items.
+	BOOL isForAllPertner; // If the state can apply its effect to all pertners.
+	unsigned short currentStateModificationPosibility[4]; // The posibility to set the state included in {state1}. The default value 0 takes the same effect as 1.
+	unsigned short basicStateModificationPosibility[7]; // The posibility to set the state included in {state2}. The default value 0 takes the same effect as 1.
+	unsigned short temporaryStateModificationPosibility[24]; // The posibility to set the state included in {state3}. The default value 0 takes the same effect as 1.
+	BOOL isInterruptCurrentAction[2]; // If the effect can interrupt the current action of {object}.
+	unsigned int specificEffectIdentifier; // Specific effect which cannot be descripted by this framwork easily.
+	
+	// MARK: Detailed effects
+	// {object} -> partners, enermies
+	// {type1} -> normal attack, skill, magic, item, sword skill, cooperated attack, all
+	// {type2} -> HP, SP, MP
+    // {property} -> water, fire, wind, thunder, earth
+	// {state1} -> current HP, current SP, current MP, money, the progress of attack sequence, skill soul, magic soul, sword skill point
+	// {state2} -> HP, SP, MP, attack, defense, speed, luck
+	// {state3} -> stable, blank, forbid, sleep, chaos, mad, mirror, wall, bound, hermit, immunity, attack inc, attack dec, defense inc, defense dec, speed inc, speed dec, luck inc, luck dec, poison water, poison fire, poison wind, poison thunder, poison earth
 
-#endif // #ifndef _PAL3_CB2DEFFECT_H_
+	// Triggered: Always
+	int dealedDamageModificationByPercent[7]; // Dealed {type1} damage + v%.
+	int dealedMagicDamageModificationByPercentWithProperty[5]; // Dealed {property} magic damage + v%.
+	int receivedDamageModificationByPercent[7]; // Recieved {type1} damage + v%.
+	int receivedMagicDamageModificationPercentWithProperty[5]; // Recieved {property} magic damage + v%.
+	int basicStateModificationFixed[7]; // {state2} + v.
+	int basicStateModificationByPercent[7]; // {state2} + v%.
+	int consumptionModificationByPercent[3]; // {type2} cost when cast skill or magic + v%.
+	int actionTakesNoEffect[8]; // {type} based action takes no effect.
+	int receivedActionTakesNoEffect[8]; // Received {type} based action takes no effect.
+	int repeatLastActionForbid; // Cannot repeat the last action. 
+
+	// Triggered: By customizing
+	int currentStateModificationWhenTriggeredFixed[8]; // Triggered: {state1} + v.
+    int currentStateModificationWhenTriggeredFixedPercent[8]; // Triggered:  {state1} + v%.
+    int currentStateModificationWhenTriggeredLevelBased[8]; // Triggered:  {state1} + [v].
+	int basicStateModificationWhenTriggeredFixed[7]; // Triggered:  {state2} + v.
+    int basicStateModificationWhenTriggeredFixedPercent[7]; // Triggered:  {state2} + v%.
+    int basicStateModificationWhenTriggeredLevelBased[7]; // Triggered:  {state2} + [v].
+	int temporaryStateModificationWhenTriggeredFixed[24]; // Triggered:  {state3} + v.
+	
+
+};
+
+struct StateDataRunningInfo
+{
+public:
+
+	unsigned int currentLayer; // The current layer value. It will be added by 1 if triggered, but cannot exceed the maximum layer.
+	unsigned int currentDuration; // The current duration which will be subtracted by 1 at the start of each round. If the duration is 0, the state will be removed.
+	unsigned int currentDelay; // The remaning rounds before the effect taking effect. If the current value is 0, the state will become effective.
+	
+	
+}
+
+#endif
