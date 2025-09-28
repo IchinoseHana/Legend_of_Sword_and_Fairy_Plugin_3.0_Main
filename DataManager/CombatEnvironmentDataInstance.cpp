e497d8d3399c8b32b0d004da0bbaf852dfeee734
Miku <53478560+mlisgood@users.noreply.github.com>
2025-09-28 21:33:40 +0900

    [State] Fix: Missing modification

--- DataManager/CombatEnvironmentDataInstance.cpp
+++ DataManager/CombatEnvironmentDataInstance.cpp
@@ -1,138 +1,136 @@
 #include "stdafx.h"
 #include "CombatEnvironmentDataInstance.h"
 
-CombatEnvironmentDataInstance::SustainableStateDataInstance()
+CombatEnvironmentDataInstance::CombatEnvironmentDataInstance():SustainableStateDataInstance()
 {
 	targetType = new bool[2]();
 	hasmask = false;
 	maskColor = new PluginColorInstance();
+	magicChangingPosibility = 0;
+	magicChangingTargetIdentifier = 0;
+	itemNotBeConsumedPosibility = new unsigned short[2]();
+	itemNotBeConsumedUpperBoundWithPrice = new unsigned int[2]();
+	dealedDamageModificationDirectly = new int[7]();
+	dealedMagicDamageModificationDirectlyWithProperty = new int[5]();
+	receivedDamageModificationDirectly = new int[7]();
+	receivedMagicDamageModificationDirectlyWithProperty = new int[5]();
+	usingRealTimeTrigger = false;
+	realTimeBasedStateModificationInterval = 0.0;
+	sustainableStateSetPosibility = new unsigned short[10]();
+	temporaryStateSetPosibility = new unsigned short[10]();
 
+	// Triggered: By customizing
+	sustainableStateSetWhenTriggeredFixed = new unsigned int[10]();
+	temporaryStateSetWhenTriggeredFixed = new unsigned int[10]();
 }
 
-CombatEnvironmentDataInstance::~SustainableStateDataInstance()
+CombatEnvironmentDataInstance::~CombatEnvironmentDataInstance()
 {
-	delete[] effectIdentifier;
-	delete[] isCenteringEffectPosition;
-	delete[] currentStateModificationPosibility;
-	delete[] basicStateModificationPosibility;
+	delete[] targetType;
+	delete maskColor;
+	delete[] itemNotBeConsumedPosibility;
+	delete[] itemNotBeConsumedUpperBoundWithPrice;
+	delete[] dealedDamageModificationDirectly;
+	delete[] dealedMagicDamageModificationDirectlyWithProperty;
+	delete[] receivedDamageModificationDirectly;
+	delete[] receivedMagicDamageModificationDirectlyWithProperty;
+	delete[] sustainableStateSetPosibility;
 	delete[] temporaryStateSetPosibility;
-	delete[] isInterruptCurrentAction;
-	delete[] customizedTriggerType;
-	delete[] cancelSourceEffect;
 
-	delete[] currentStateModificationWhenTriggeredFixed;
-	delete[] currentStateModificationWhenTriggeredByPercent;
-	delete[] currentStateModificationWhenTriggeredLevelBased;
-	delete[] basicStateModificationWhenTriggeredFixed;
-	delete[] basicStateModificationWhenTriggeredByPercent;
-	delete[] basicStateModificationWhenTriggeredLevelBased;
+	delete[] sustainableStateSetWhenTriggeredFixed;
 	delete[] temporaryStateSetWhenTriggeredFixed;
 }
 
-string TemporaryStateDataInstance::printData(stringstream ss)
+string CombatEnvironmentDataInstance::printData(stringstream ss)
 {
+	// Get the result from the super class
+	string tempStr = SustainableStateDataInstance::printData(ss);
+
 	ss.str("");
     ss.clear();
 
 	int index = 0;
-	ss << "-----TemporaryStateDataInstance Print Begin-----\n";
-	ss << "identifier: " << n2s(identifier) << "\n";
-	ss << "name: " << name << "\n";
-	ss << "description: " << description << "\n";
-	ss << "effectIdentifier: ";
+	ss << "-----CombatEnvironmentDataInstance Print Begin-----\n";
+	// Information from the super class
+	ss << "-----Information From Class SustainableStateDataInstance: Print Begin-----\n";
+	ss << tempStr;
+	ss << "-----Information From Class SustainableStateDataInstance: Print End-----\n";
+	ss << "-----Information From Class CombatEnvironmentDataInstance: Print Begin-----\n";
+
+	ss << "targetType: ";
+	for (index = 0; index < 2; ++index)
+	{
+		ss << n2s(targetType[index]) << " ";
+	}
+	ss << "\n";
+	ss << "hasmask: " << n2s(hasmask) << "\n";
+	ss << "maskColor: " << n2s(maskColor->red) << " " << n2s(maskColor->green) << " " << n2s(maskColor->blue) << " " << n2s(maskColor->alpha) << "\n";
+	ss << "magicChangingPosibility: " << n2s(magicChangingPosibility) << "\n";
+	ss << "magicChangingTargetIdentifier: " << n2s(magicChangingTargetIdentifier) << "\n";
+	ss << "itemNotBeConsumedPosibility: ";
+	for (index = 0; index < 2; ++index)
+	{
+		ss << n2s(itemNotBeConsumedPosibility[index]) << " ";
+	}
+	ss << "\n";
+	ss << "itemNotBeConsumedUpperBoundWithPrice: ";
+	for (index = 0; index < 2; ++index)
+	{
+		ss << n2s(itemNotBeConsumedUpperBoundWithPrice[index]) << " ";
+	}
+	ss << "\n";
+	ss << "dealedDamageModificationDirectly: ";
+	for (index = 0; index < 7; ++index)
+	{
+		ss << n2s(dealedDamageModificationDirectly[index]) << " ";
+	}
+	ss << "\n";
+	ss << "dealedMagicDamageModificationDirectlyWithProperty: ";
+	for (index = 0; index < 5; ++index)
+	{
+		ss << n2s(dealedMagicDamageModificationDirectlyWithProperty[index]) << " ";
+	}
+	ss << "\n";
+	ss << "receivedDamageModificationDirectly: ";
+	for (index = 0; index < 7; ++index)
+	{
+		ss << n2s(receivedDamageModificationDirectly[index]) << " ";
+	}
+	ss << "\n";
+	ss << "receivedMagicDamageModificationDirectlyWithProperty: ";
+	for (index = 0; index < 5; ++index)
+	{
+		ss << n2s(receivedMagicDamageModificationDirectlyWithProperty[index]) << " ";
+	}
+	ss << "\n";
+	ss << "usingRealTimeTrigger: " << n2s(usingRealTimeTrigger) << "\n";
+	ss << "realTimeBasedStateModificationInterval: " << n2s(realTimeBasedStateModificationInterval) << "\n";	
+	ss << "sustainableStateSetPosibility: ";
 	for (index = 0; index < 10; ++index)
 	{
-		ss << n2s(effectIdentifier[index]) << " ";
-	}
-	ss << "\n";
-	ss << "isCenteringEffectPosition: ";
-	for (index = 0; index < 10; ++index)
-	{
-		ss << n2s(isCenteringEffectPosition[index]) << " ";
-	}
-	ss << "\n";
-	ss << "slot: " << n2s(slot) << "\n";
-	ss << "isForAllPartner: " << n2s(isForAllPartner) << "\n";
-	ss << "currentStateModificationPosibility: ";
-	for (index = 0; index < 8; ++index)
-	{
-		ss << n2s(currentStateModificationPosibility[index]) << " ";
-	}
-	ss << "\n";
-	ss << "basicStateModificationPosibility: ";
-	for (index = 0; index < 12; ++index)
-	{
-		ss << n2s(basicStateModificationPosibility[index]) << " ";
+		ss << n2s(sustainableStateSetPosibility[index]) << " ";
 	}
 	ss << "\n";
 	ss << "temporaryStateSetPosibility: ";
-	for (index = 0; index < 24; ++index)
+	for (index = 0; index < 10; ++index)
 	{
 		ss << n2s(temporaryStateSetPosibility[index]) << " ";
 	}
 	ss << "\n";
-	ss << "isInterruptCurrentAction: ";
-	for (index = 0; index < 2; ++index)
+	ss << "sustainableStateSetWhenTriggeredFixed: ";
+	for (index = 0; index < 10; ++index)
 	{
-		ss << n2s(isInterruptCurrentAction[index]) << " ";
-	}
-	ss << "\n";
-	ss << "specificEffectIdentifier: " << n2s(specificEffectIdentifier) << "\n";
-	ss << "customizedTriggerType: ";
-	for (index = 0; index < 7; ++index)
-	{
-		ss << n2s(customizedTriggerType[index]) << " ";
-	}
-	ss << "\n";
-	ss << "cancelSourceEffect: ";
-	for (index = 0; index < 7; ++index)
-	{
-		ss << n2s(cancelSourceEffect[index]) << " ";
-	}
-	ss << "\n";
-	ss << "currentStateModificationWhenTriggeredFixed: ";
-	for (index = 0; index < 8; ++index)
-	{
-		ss << n2s(currentStateModificationWhenTriggeredFixed[index]) << " ";
-	}
-	ss << "\n";
-	ss << "currentStateModificationWhenTriggeredByPercent: ";
-	for (index = 0; index < 8; ++index)
-	{
-		ss << n2s(currentStateModificationWhenTriggeredByPercent[index]) << " ";
-	}
-	ss << "\n";
-	ss << "currentStateModificationWhenTriggeredLevelBased: ";
-	for (index = 0; index < 8; ++index)
-	{
-		ss << n2s(currentStateModificationWhenTriggeredLevelBased[index]) << " ";
-	}
-	ss << "\n";
-	ss << "basicStateModificationWhenTriggeredFixed: ";
-	for (index = 0; index < 12; ++index)
-	{
-		ss << n2s(basicStateModificationWhenTriggeredFixed[index]) << " ";
-	}
-	ss << "\n";
-	ss << "basicStateModificationWhenTriggeredByPercent: ";
-	for (index = 0; index < 12; ++index)
-	{
-		ss << n2s(basicStateModificationWhenTriggeredByPercent[index]) << " ";
-	}
-	ss << "\n";
-	ss << "basicStateModificationWhenTriggeredLevelBased: ";
-	for (index = 0; index < 12; ++index)
-	{
-		ss << n2s(basicStateModificationWhenTriggeredLevelBased[index]) << " ";
+		ss << n2s(sustainableStateSetWhenTriggeredFixed[index]) << " ";
 	}
 	ss << "\n";
 	ss << "temporaryStateSetWhenTriggeredFixed: ";
-	for (index = 0; index < 24; ++index)
+	for (index = 0; index < 10; ++index)
 	{
 		ss << n2s(temporaryStateSetWhenTriggeredFixed[index]) << " ";
 	}
 	ss << "\n";
-	ss << "-----TemporaryStateDataInstance Print End-----\n";
+	ss << "-----Information From Class CombatEnvironmentDataInstance: Print End-----\n";
+	ss << "-----CombatEnvironmentDataInstance Print End-----\n";
 
 	return ss.str();
 }
